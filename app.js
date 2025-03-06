03ca2d2d2df4b4bc38551dc1b8ce6da1229a6486
Madina <madi.albarn@gmail.com>
2025-03-06 22:04:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ let addressLong = 9;
 let positionLat = 5;
 let positionLong = 3;
 const width = addressLat - positionLat;
-const lenth = addressLong - positionLong;
-const distance = Math.sqrt((width ** 2 + lenth ** 2));
+const length = addressLong - positionLong;
+const distance = Math.sqrt((width ** 2 + length ** 2));
 console.log(distance);
