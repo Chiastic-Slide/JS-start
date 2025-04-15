43d77ba4e9154ad7b2540e1d3ca24085c9c9b732
Madina <madi.albarn.com>
2025-04-15 16:05:00 +0300

    12-luna

---
+++ 12-app.js
@@ -0,0 +1,20 @@
+// Помогите, пожалуйста, с проверкой на тип элемента и количеством передаваемых символов (если необходимо).
+const card = '4561-2612-1234-5464';
+
+const theLuhnAlgorithm = string => {
+	const cardNumber = string.replaceAll('-', '').split('').map(el => Number(el)); // 
+	let oddNumbers = [];
+	let evenNumbers = [];
+		for (let i = 0; i < cardNumber.length; i += 2) {
+			oddNumbers.push(cardNumber[i]);
+		}
+		oddNumbers = oddNumbers.map(el => (el * 2 > 9 ? el * 2 - 9 : el * 2))
+		.reduce((acc, value) => acc + value, 0);
+		for (let i = 1; i < cardNumber.length; i += 2) {
+			evenNumbers.push(cardNumber[i]);
+		}
+		evenNumbers = evenNumbers.reduce((acc, value) => acc + value, 0);
+		return (oddNumbers + evenNumbers) % 10 === 0;
+	}
+			
+console.log(theLuhnAlgorithm(card));
